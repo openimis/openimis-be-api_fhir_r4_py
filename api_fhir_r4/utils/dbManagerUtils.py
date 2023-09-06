54c3bb26eac631ec8718467161c1276e5082be6b
Jan <j.dolkowski@soldevelo.com>
2023-09-06 17:21:12 +0200

    OP-1522: if code is an identifier, use valididty_to

--- api_fhir_r4/utils/dbManagerUtils.py
+++ api_fhir_r4/utils/dbManagerUtils.py
@@ -4,15 +4,19 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 
 
 class DbManagerUtils(object):
+    CODE_IDENTIFIER = 'code'
 
     __FIRST = 0
 
     @classmethod
     def get_object_or_none(cls, model, **kwargs):
+        if cls.CODE_IDENTIFIER in kwargs:
+            kwargs["validity_to__isnull"] = True
+
         try:
             result = get_object_or_404(model, **kwargs)
         except MultipleObjectsReturned:
-            result = get_list_or_404(model, validity_to__isnull=True, **kwargs)[cls.__FIRST]
+            result = get_list_or_404(model, **kwargs)[cls.__FIRST]
         except Http404:
             result = None
         return result
