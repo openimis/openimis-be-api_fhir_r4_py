d401f14b0d140d7609263db88010855c54a7097e
delcroip <patrick.delcroix@swisstph.ch>
2024-03-01 18:47:33 +0100

    fix migration

--- api_fhir_r4/migrations/0004_update_subscription_criteria.py
+++ api_fhir_r4/migrations/0004_update_subscription_criteria.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.db import migrations
+from django.db.models import Q
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +16,7 @@ def update_subscription_criteria(apps, schema_editor):
 
     subscription_model = apps.get_model('api_fhir_r4', 'Subscription')
     entries_to_update = subscription_model.objects\
-        .exclude(criteria__has_key='resource')\
-        .filter(criteria__has_key='resource_type')
+        .filter(Q(criteria__has_key='resource_type')  & ~Q(criteria__has_key='resource'))
     for subscription in entries_to_update:
         resource = subscription.criteria.pop('resource_type')
         if resource in resource_type_map:
