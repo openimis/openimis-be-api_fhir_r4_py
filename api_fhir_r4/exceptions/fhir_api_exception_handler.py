9f63fab7d47f71b4f6dc2347bf64d2080e3b50c6
delcroip <patrick.delcroix@swisstph.ch>
2024-10-19 23:48:08 +0200

    show trace if error

--- api_fhir_r4/exceptions/fhir_api_exception_handler.py
+++ api_fhir_r4/exceptions/fhir_api_exception_handler.py
@@ -1,7 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import exceptions, status, views
+from api_fhir_r4.exceptions import FHIRException
 
+from django.conf import settings
+import traceback
+import logging
+
+logger = logging.getLogger(__name__)
 
 def call_default_exception_handler(exc, context):
     # Call REST framework's default exception handler first, to get the standard error response.
@@ -20,8 +26,18 @@ def fhir_api_exception_handler(exc, context):
     if 'api_fhir_r4' in request_path:
         from api_fhir_r4.converters import OperationOutcomeConverter
         fhir_outcome = OperationOutcomeConverter.to_fhir_obj(exc)
+        if settings.DEBUG and not isinstance(exc, (
+            exceptions.NotAuthenticated,
+            exceptions.AuthenticationFailed,
+            exceptions.PermissionDenied,
+            FHIRException
+        )):
+            trace = traceback.extract_tb(traceback.sys.exc_info()[2])
+            logger.debug("Unexpected {exc.__class__.__name__} trace:\n" + ''.join(traceback.format_list(trace))) 
+
         if not response:
             response = __create_server_error_response()
+      
         response.data = fhir_outcome.dict()
 
     return response
