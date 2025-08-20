04429a5695c2f1deb61bec11ffabd8124cb550d3
Patrick Delcroix <delcroip@gmail.com>
2025-08-20 16:35:05 +0200

    chore: test fix

--- api_fhir_r4/tests/subscriptions/manager.py
+++ api_fhir_r4/tests/subscriptions/manager.py
@@ -1,6 +1,6 @@
 import datetime
 
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, MagicMock
 from django.test import TestCase
 
 from api_fhir_r4.converters import ClaimConverter, ReferenceConverterMixin
